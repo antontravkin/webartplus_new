5c2264dff6a4f1c340a7eecd40e6e740c081a8de
Anton Travkin <travkin.rh@gmail.com>
2025-02-16 12:23:38 +0300

    modx

---
+++ www/webartplus.ru/core/cache/namespaces/namespaces.cache.php
@@ -0,0 +1,62 @@
+<?php  return array (
+  'ace' => 
+  array (
+    'name' => 'ace',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/ace/',
+    'assets_path' => '',
+  ),
+  'ajaxform' => 
+  array (
+    'name' => 'ajaxform',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/ajaxform/',
+    'assets_path' => '',
+  ),
+  'core' => 
+  array (
+    'name' => 'core',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/manager/',
+    'assets_path' => '/var/www/u1833722/data/www/webartplus.ru/manager/assets/',
+  ),
+  'filetranslit' => 
+  array (
+    'name' => 'filetranslit',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/filetranslit/',
+    'assets_path' => '',
+  ),
+  'formit' => 
+  array (
+    'name' => 'formit',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/formit/',
+    'assets_path' => '/var/www/u1833722/data/www/webartplus.ru/assets/components/formit/',
+  ),
+  'migx' => 
+  array (
+    'name' => 'migx',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/migx/',
+    'assets_path' => '/var/www/u1833722/data/www/webartplus.ru/assets/components/migx/',
+  ),
+  'pdotools' => 
+  array (
+    'name' => 'pdotools',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/pdotools/',
+    'assets_path' => '',
+  ),
+  'phpthumbon' => 
+  array (
+    'name' => 'phpthumbon',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/phpthumbon/',
+    'assets_path' => '',
+  ),
+  'tinymce' => 
+  array (
+    'name' => 'tinymce',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/tinymce/',
+    'assets_path' => '/var/www/u1833722/data/www/webartplus.ru/assets/components/tinymce/',
+  ),
+  'translit' => 
+  array (
+    'name' => 'translit',
+    'path' => '/var/www/u1833722/data/www/webartplus.ru/core/components/translit/',
+    'assets_path' => '',
+  ),
+);
